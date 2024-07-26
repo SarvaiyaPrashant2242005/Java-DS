cb07ddb233ee63da0116aa43ee4a76548e97502c
SarvaiyaPrashant2242005 <115221462+SarvaiyaPrashant2242005@users.noreply.github.com>
2024-07-26 08:02:39 +0530

    Update Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -74,7 +74,22 @@ public class Pattern {
             }
             System.out.println();
         }
-
+    // ***********
+    // *         *
+    // ***********
+        int j;
+    for(int i=1;i<=4;i++){
+        for(j=1;j<=5;j++){
+            // cell = i,j
+            if(i == 1 || i == 4 || j == 1 || j == 5){
+                System.out.print("*");
+            }
+            else{
+                System.out.println();
+            }
+        }
+        System.out.println();
+    }
    }
 }
 
