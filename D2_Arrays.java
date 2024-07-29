f34e6829395f138d5497aa7e089cd52f7a08f4c6
SarvaiyaPrashant2242005 <115221462+SarvaiyaPrashant2242005@users.noreply.github.com>
2024-07-29 19:41:59 +0530

    2D arrays

--- D2_Arrays.java
+++ D2_Arrays.java
@@ -1,3 +1,44 @@
+import java.util.Scanner;
+
 public class D2_Arrays {
-    
+    public static void search(int[][] array, int key) {
+        boolean found = false;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (array[i][j] == key) {
+                    System.out.println(key + " found at cell number (" + i + ", " + j + ")");
+                    found = true;
+                }
+            }
+        }
+        if (!found) {
+            System.out.println("Key not found in the given array");
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 3, m = 3;
+        int key = 4;
+
+        int[][] array = new int[n][m];
+        Scanner sc = new Scanner(System.in);
+
+        // Input elements
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print("Element (" + i + "," + j + ") : ");
+                array[i][j] = sc.nextInt();
+            }
+        }
+
+        // Output elements
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();  // New line after each row
+        }
+
+        search(array, 9);
+    }
 }
